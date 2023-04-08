c94a3b8787e9e85351ab7f1647327a9be0551b42
CaioMenaBarreto <clpmb2000@gmail.com>
2023-04-08 20:06:53 -0300

    jogo pronto e responsivo

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 let quantCartas = prompt("Com quantas cartas deseja jogar?");
 
-=======
-// Solicita ao usuário o número de cartas a serem usadas no jogo
-let quantCartas = prompt("Com quantas cartas deseja jogar?");
-
-// Executa um loop até que o usuário insira um número válido de cartas (entre 4 e 14, e com quantidade par)
->>>>>>> 07c7513cba291fd6b3d49956de3f282aef1d01a7
 while (quantCartas > 14 || quantCartas < 4 || quantCartas % 2 !== 0) {
   quantCartas = prompt("Com quantas cartas deseja jogar?");
 }
 
-<<<<<<< HEAD
 const main = document.querySelector("main");
 const cartaInicial = document.querySelector(".carta-jogo");
 
@@ -33,12 +25,12 @@ todasAsImagens.sort(() => Math.random() - 0.5);
 
 for (let i = 0; i < quantCartas; i++) {
   main.innerHTML += `
-  <div  class ="carta-jogo" >
- <div class="front-face face">
-     <img src="./imagens-uteis/back.png" alt="">
+  <div  class ="carta-jogo" data-test="card">
+ <div class="front-face face" >
+     <img src="./imagens-uteis/back.png" data-test="face-down-image" alt="">
    </div>
    <div class="back-face face">
-     <img src="${todasAsImagens[i]}" alt="">
+     <img src="${todasAsImagens[i]}" data-test="face-up-image" alt="">
  </div>
 </div>`
 }
@@ -89,96 +81,3 @@ cartas.forEach((carta) => {
     }
   });
 })
-
-=======
-// Seleciona o elemento <main> do documento HTML e a carta de jogo inicial (que será removida)
-const main = document.querySelector("main");
-const cartaInicial = document.querySelector(".carta-jogo");
-cartaInicial.remove();
-
-// Cria um array com as URLs das imagens dos papagaios que serão usados no jogo
-const imagens = [
-    '././projeto__parrots__imagens (1)/imagens-uteis/bobrossparrot.gif',
-    './projeto__parrots__imagens (1)/imagens-uteis/explodyparrot.gif',
-    './projeto__parrots__imagens (1)/imagens-uteis/fiestaparrot.gif',
-    './projeto__parrots__imagens (1)/imagens-uteis/metalparrot.gif',
-    './projeto__parrots__imagens (1)/imagens-uteis/revertitparrot.gif',
-    './projeto__parrots__imagens (1)/imagens-uteis/tripletsparrot.gif',
-    './projeto__parrots__imagens (1)/imagens-uteis/unicornparrot.gif'
-];
-
-// Seleciona a metade das imagens de papagaios que serão usadas no jogo (de acordo com o número de cartas)
-const imagensSelecionadas = imagens.slice(0, quantCartas / 2);
-
-// Cria um novo array com as imagens selecionadas, duplicando-as para que haja pares de cartas
-const todasAsImagens = imagensSelecionadas.concat(imagensSelecionadas);
-
-// Embaralha o array de imagens aleatoriamente
-todasAsImagens.sort(() => Math.random() - 0.5);
-
-// Cria as cartas do jogo, adicionando uma imagem de papagaio de costas a cada uma delas
-for (let i = 0; i < quantCartas; i++) {
-  const novaCarta = document.createElement("div");
-  novaCarta.className = "carta-jogo";
-  novaCarta.innerHTML =
-    '<img class="papagaio" src="./projeto__parrots__imagens (1)/imagens-uteis/back.png" alt="">';
-  novaCarta.setAttribute("data-image", todasAsImagens[i]);
-  main.appendChild(novaCarta);
-}
-
-// Seleciona todas as cartas do jogo e adiciona um evento de clique a cada uma delas
-const cartas = document.querySelectorAll(".carta-jogo");
-
-cartas.forEach((carta) => {
-  carta.addEventListener("click", () => {
-    if (!carta.classList.contains("virada")) {
-      carta.classList.add("virada");
-      const imagem = carta.getAttribute("data-image");
-      const papagaio = carta.querySelector(".papagaio");
-      papagaio.setAttribute("src", imagem);
-    }
-  });
-});
-
-
-
-/*let primeiraCartaVirada = null;
-let segundaCartaVirada = null;
-
-function verificarIguais() {
-  if (
-    primeiraCartaVirada !== null &&
-    segundaCartaVirada !== null &&
-    primeiraCartaVirada.getAttribute('data-image') === segundaCartaVirada.getAttribute('data-image')
-  ) {
-    primeiraCartaVirada.removeEventListener('click', clicarCarta);
-    segundaCartaVirada.removeEventListener('click', clicarCarta);
-    primeiraCartaVirada = null;
-    segundaCartaVirada = null;
-  } else if (primeiraCartaVirada !== null && segundaCartaVirada !== null) {
-    setTimeout(() => {
-      primeiraCartaVirada.classList.remove('virada');
-      segundaCartaVirada.classList.remove('virada');
-      primeiraCartaVirada = null;
-      segundaCartaVirada = null;
-    }, 1000);
-  }
-}
-
-function clicarCarta() {
-  if (!this.classList.contains('virada')) {
-
-    if (primeiraCartaVirada === null) {
-      primeiraCartaVirada = this;
-    } else {
-      segundaCartaVirada = this;
-      verificarIguais();
-    }
-
-    this.classList.add('virada');
-
-  }
-}
-
-cartas.forEach(carta => carta.addEventListener('click', clicarCarta));*/
->>>>>>> 07c7513cba291fd6b3d49956de3f282aef1d01a7
